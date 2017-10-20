b973484714b7fa58afcd1baf47ca9743e06b72d3
daewon <daewon@apache.org>
2017-10-21 00:57:15 +0900

    add hello world

--- daewon/no_1/src/Lib.hs
+++ daewon/no_1/src/Lib.hs
@@ -1,6 +1,16 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Lib (no1) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.IO
+import qualified Data.Text as T
+
+prompt :: String -> IO String
+prompt msg = putStr msg >> hFlush stdout >> getLine
+
+welcome :: String -> String
+welcome name = "Hello, " ++ name ++ ", " ++ "nice to meet you"
+
+no1 :: IO ()
+no1 = do
+  name <- prompt "What is your name? "
+  putStrLn $ welcome name
