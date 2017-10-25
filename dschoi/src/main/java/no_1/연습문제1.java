29e807a734bdc254cbddb319f291e6da6b95f1ab
setin82@gmail.com <setin82@gmail.com>
2017-10-25 22:34:36 +0900

    git
    junit 테스트 케이스를 하려했는데 system in 은 junit 테스특 안되넹..

---
+++ dschoi/src/main/java/no_1/연습문제1.java
@@ -0,0 +1,37 @@
+package no_1;
+
+import com.sun.javafx.binding.StringFormatter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.SyncFailedException;
+import java.util.Scanner;
+
+/**
+ * Created by 두석 on 2017-10-25.
+ */
+public class 연습문제1 {
+
+    public static final String HELLO_STRING = "안녕  %s , 반가워";
+
+    public 연습문제1(){
+
+        scanner = new Scanner(System.in);
+    }
+
+    public void init(){
+        System.out.print("What's your name?");
+    }
+
+    private Scanner scanner;
+
+    public String input() throws IOException {
+        return scanner.next();
+    }
+
+    public void print(String inputData){
+        System.out.println(String.format(HELLO_STRING, inputData ));
+    }
+
+}
---
+++ dschoi/src/test/java/no_1/연습문제_test.java
@@ -0,0 +1,17 @@
+package no_1;
+
+import java.io.IOException;
+
+/**
+ * Created by 두석 on 2017-10-25.
+ */
+public class 연습문제_test {
+
+
+    public static void main(String[] args) throws IOException {
+        연습문제1 연습문제 = new 연습문제1();
+        연습문제.init();
+        String input = 연습문제.input();
+        연습문제.print(input);
+    }
+}
